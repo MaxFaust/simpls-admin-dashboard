d824b5f0225924f58c097794e66d3fed939cf3c0
MAX FAUST <maxfaust@MAXs-MacBook-Pro.local>
2022-01-15 21:17:07 -0600

    home page, widgetLg, and sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
+import "./App.css";
+import Home from "./pages/home/Home";
+// import { BrowserRouter as Route, Switch } from "react-router-dom";
+// import UserList from "./pages/userList/UserList";
+// import User from "./pages/user/User";
+// import NewUser from "./pages/newUser/NewUser";
+// import ProductList from "./pages/productList/ProductList";
+// import Product from "./pages/product/Product";
+// import NewProduct from "./pages/newProduct/NewProduct";
 
 function App() {
   return (
-    <div>
+    <>
       <Topbar />
-    </div>
+      <div className="container">
+        <Sidebar />
+            <Home />
+      </div>
+    </>
+
   );
 }
 
